f10e3b86bcf4957c1bb0fef55550071d216e4828
userlog404 <101346110+userlog404@users.noreply.github.com>
2022-08-01 15:23:41 +0300

    t

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,7 @@ is an instance of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False.
 """
 
+
 def inherits_from(obj, a_class) -> bool:
     """implementation
     """
